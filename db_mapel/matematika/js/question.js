7f76020cf5ec213b28317e7bc8d8d8e589b96eda
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2024-01-17 16:05:31 +0700

    Check Point

--- db_mapel/matematika/js/question.js
+++ db_mapel/matematika/js/question.js
@@ -2,302 +2,328 @@
 const quiz = [
   // Nomer 1
   {
-    q: "Gerakan DI/TII di Sulawesi Selatan dipimpin oleh:",
+    q: "Jika 8<sup>(2x+1)</sup>=32sup>(2x+1)</sup>, maka berapakah nilai x yang memenuhi adalah",
     options: [
-      "SM. Karto Suwirjo",
-      "Ibnu Hajar alias Haderi",
-      "Amir Fattah",
-      "Kahar Muzakkar",
-      "Mayor Alfredo",
+      "1/2",
+      "-1/2",
+      "2",
+      "-2",
+      "3/2",
     ],
-    answer: 3,
+
+    answer: 1,
   },
-  // Nomer 2
-  {
-    q: "Polisi yang salah satu tugas pokoknya melakukan penyidikan terhadap tindak pidana umum adalah fungsi dari",
-    options: [
-      "Sat-Binmas",
-      "Sabhara",
-      "Densus 88",
-      "Polair",
-      "Sat-Reskrim",
-    ],
-    answer: 4,
-  },
+    // Nomer 2
+    {
+      q: "	Bentuk sederhana dari (3<sup>2</sup>xy<sup>3</sup>/3<sup>3</sup>x<sup>-1</sup>y<sup>2</sup>)<sup>-2</sup> adalah...",
+      options: [
+        "-9/xy<sup>2</sup> ",
+        "9/x<sup>4</sup>y<sup>2</sup> )",
+        "-x<sup>4</sup>y<sup>2</sup>)/9",
+        "x^<sup>2</sup>y/9",
+        "9/xy<sup>2</sup> ",
+      ],
+  
+      answer: 1,
+    },
   // Nomer 3
   {
-    q: "Indonesia merupakan salah satu negara dengan populasi terbanyak di Dunia. Indonesia menempati urutan ke-… sebagai negara dengan jumlah penduduk terbanyak dengan perkiraan jumlah penduduk sebesar 270 Juta Penduduk.",
+    q: "Kondisi fungsi kuadrat  f(x)=x<sup>2</sup>+2x-3 pada titik koordinat titik puncaknya adalah...",
     options: [
-      "1",
-      "2",
+      "(1,4) kondisi minimum",
+      "(-1,4) kondisi maksimum",
+      "(-4,1) kondisi maksimum",
+      "(-4,-1) kondisi minimum",
+      "(1,4) kondisi maksimum",
+    ],
+
+    answer: 3,
+  },
+    // Nomer 5
+    {
+      q: "Jika akar-akar persamaan kuadrat x<sup>2</sup>-10x+5=0 adalah m dan n, maka nilai dari m+n/mn adalah...",
+      options: [
+        "-2",
+        "-1/2",
+        "1/2",
+        "2",
+        "4",
+      ],
+  
+      answer: 3,
+    },
+      // Nomer 6
+  {
+    q: "Persamaan kuadrat x<sup>2</sup>+ax-18=0, salah satu akar-akar persamaanya adalah x=-9. Maka nilai a yang sesuai pada persamaan di atas adalah...",
+    options: [
+      "-7",
+      "7",
+      "11",
+      "-11",
+      "18",
+    ],
+
+    answer: 1,
+  },
+    // Nomer 7
+    {
+      q: "Claresta, Lucky dan Roy pergi ke Toko Alang-Alang untuk membeli alat tulis. Jika Claresta membeli 3 buku tulis dan 5 pulpen dengan total harga Rp43.000,00. Lucky membeli 4 buku tulis dan 2 pulpen dengan membayar sebesar Rp34.000,00. Jika Roy membeli 5 buku tulis dan 7 pulpen yang berjenis sama di Toko Alang-Alang, maka ia harus membayar sebesar... ",
+      options: [
+        "Rp65.000,00",
+        "Rp55.000,00",
+        "Rp70.000,00",
+        "Rp40.000,00",
+        "Rp80.000,00",
+      ],
+  
+      answer: 0,
+    },
+    // Nomer 9
+    {
+      q: "Diketahui bahwa Riza 6 tahun lebih tua dari Ani. Diketahui juga bahwa Ani 3 tahun lebih tua dari Dina. Jika jumlah umur Riza, Ani, dan Dina adalah 72 tahun, maka berapakah jumlah umur Riza dan Dina?",
+      options: [
+        "59 tahun",
+        "49 tahun",
+        "66 tahun",
+        "48 tahun",
+        "56 tahun",
+      ],
+  
+      answer: 1,
+    },
+    // Nomer 10
+    {
+      q: "	Jika f(x)=3x+6 dan g(x)=(x+3)/(2x-1),x≠1/2 maka (g∘f)(x)=",
+      options: [
+        "11x+9/6x-3",
+        "3x+3/3x+5",
+        "3x-9/6x-11",
+        "4x+9/5x+5",
+        "3x+9/6x+11",
+      ],
+  
+      answer: 4,
+    },
+    // Nomer 11
+    {
+      q: "	Diketahui f(x)=(3x+2)/(2x-5),x≠5/2. Maka nilai f<sup>(-1)</sup> (8)=",
+      options: [
+        "-3",
+        "1/4",
+        "4",
+        "-4",
+        "3",
+      ],
+  
+      answer: 2,
+    },
+      // Nomer 13
+  {
+    q: "Seorang pembuat kue mempunyai 8 kg tepung dan 2 kg gula pasir. Ia ingin membuat dua macam kue yaitu kue dadar dan kue apem. Untuk membuat kue dadar dibutuhkan 10 gram gula pasir dan 20 gram tepung sedangkan untuk membuat sebuah kue apem dibutuhkan 5 gram gula pasir dan 50 gram tepung. Jika kue dadar dijual dengan harga Rp300,00/buah dan kue apem dijual dengan harga Rp500,00/buah, pendapatan maksimum yang didapat oleh pembuat kue tersebut adalah",
+    options: [
+      "Rp80.000,00",
+      "Rp95.000,00",
+      "Rp60.000,00",
+      "Rp1500.000,00",
+      "Rp100.000,00",
+    ],
+
+    answer: 1,
+  },
+    // Nomer 15
+    {
+      q: "Tempat duduk gedung pertunjukan film diatur mulai dari baris depan ke belakang dengan banyak baris di belakang lebih 4 kursi dari baris di depannya. Bila dalam gedung pertunjukan itu terdapat 15 baris kursi dan baris terdepan ada 20 kursi, kapasitas gedung tersebut adalah ⋯⋅",
+      options: [
+        "1.200 kursi",
+        "800 kursi",
+        "600 kursi",
+        "720 kursi",
+        "300 kursi",
+      ],
+  
+      answer: 3,
+    },
+      // Nomer 16
+  {
+    q: "Rizkiani melakukan tryout esay yang harus mengerjakan 8 dari 12 soal yang disediakan. Jika terdapat syarat soal nomor 4 dan 8 harus dikerjakan, maka berapakah kemungkinan Rizkiani untuk memilih sisa soal untuk dikerjakan?",
+    options: [ 
+      "66",
+      "200",
+      "205",
+      "210",
+      "495",
+    ],
+
+    answer: 3,
+  },
+    // Nomer 17
+    {
+      q: "Kania mempunyai dua kotak dilabeli A dan B yang berisi bola hijau, hitam, putih, dan merah. Kotak A berisi 5 bola hijau dan 3 bola hitam, sedangkan di dalam kotak B berisi 4 bola putih dan 4 bola merah. Masing-masing kotak akan diambil satu bola secara acak. Berapakah peluang terambilnya bola hitam dari kotak A dan bola putih dari kotak B?",
+      options: [
+        "3/64",
+        "7/8",
+        "3/16",
+        "3/4",
+        "7/64",
+      ],
+  
+      answer: 2,
+    },
+      // Nomer 18
+  {
+    q: "Bintang melempar dua buah dadu secara bersamaan.Berapakah peluang munculnya dua mata dadu yang kembar atau berjumlah 7?",
+    options: [
+      "1/6",
+      "2/3",
+      "1/2",
+      "5/6",
+      "1/3",
+    ],
+
+    answer: 4,
+  },
+    // Nomer 19
+    {
+      q: "Berapakah nilai simpangan rata-rata dari data: 4, 7, 6, 7, 7, 8, 6, 3?",
+      options: [
+        "1,0",
+        "1,25",
+        "2,0",
+        "2,25",
+        "2,5",
+      ],
+  
+      answer: 1,
+    },
+    // Nomer 21
+    {
+      q: "Nilai dari lim(x→2)⁡(4x-8)/(x<sup>2</sup>-4)=",
+      options: [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+      ],
+  
+      answer: 0,
+    },
+    // Nomer 22
+    {
+      q: "Nilai dari lim(x→∞)⁡√(2xsup>2</sup>+5x+6)-√(2xsup>2</sup>+2x-1)=",
+      options: [
+        "2√2",
+        "3√2",
+        "3/4√2",
+        "3/2√2",
+        "4√2",
+      ],
+  
+      answer: 2,
+    },
+      // Nomer 23
+  {
+    q: "Diberikan fungsi f(x)=6x<sup>4</sup>-2x<sup>3</sup>+3x<sup>2</sup>-x-3 dan f'(x) merupakan turunan pertama dari f(x). Berapakah nilai f'(1)?",
+    options: [
+      "20",
+      "21",
+      "23",
+      "24",
+      "26",
+    ],
+
+    answer: 2,
+  },
+    // Nomer 24
+    {
+      q: "Fungsi f(x)=2x<sup>3</sup>-3x<sup>2</sup>-12x+7 turun untuk x yang memenuhi pada rentang ....",
+      options: [
+        "-2 < x < 1",
+        "-3 < x < -1",
+        "-2 < x < 3",
+        "-1 < x < 2",
+        "-1 < x < -3",
+      ],
+  
+      answer: 3,
+    },
+    // Nomer 25
+    {
+      q: "Berapakah hasil pengintegralan dari ∫(12x<sup>5</sup> -8x<sup>3</sup>-4x) dx?",
+      options: [
+        "3x<sup>6</sup>-2x<sup>4</sup>-2x<sup>2</sup>+C",
+        "2x<sup>6</sup>-4/3 x<sup>4</sup>-2x<sup>2</sup>+C",
+        "2x<sup>6</sup>-2x<sup>4</sup>-2x<sup>2</sup>+C",
+        "2x^<sup>6</sup>-2x<sup>4</sup>-x<sup>2</sup>+C",
+        "2x<sup>6</sup>-2x<sup>3</sup>-x<sup>2</sup>+C",
+      ],
+  
+      answer: 2,
+    },
+    // Nomer 26
+    {
+      q: "Berapakah hasil pengintegralan dari ∫<sup>4</sup><sub>2</sub>(6x<sup>2</sup>-6x-1)dx?",
+      options: [
+        "64",
+        "68",
+        "72",
+        "74",
+        "76",
+      ],
+  
+      answer: 3,
+    },
+      // Nomer 27
+      {
+        q: "Diberikan bahwa cos⁡θ=6/7, yang mana θ adalah sudut lancip. Maka berapakah nilai cosec⁡θ?",
+        options: [
+          "7/6",
+          "1/√13",
+          "√13",
+          "7/6 √13  ",
+          "7/13 √13",
+        ],
+    
+        answer: 4,
+      },
+      // Nomer 28
+  {
+    q: "Jika putri menaruh tongkat dan mencoba untuk membentuk sudut elevasi 60° dan jaraknya 6 meter. Berapakah tinggi pohonnya?",
+    options: [
+      "5 meter",
+      "6 meter",
+      "6√3 meter",
+      "3√6 meter",
+      "5√6 meter",
+    ],
+
+    answer: 2,
+  },
+      // Nomer 29
+  {
+    q: "Diketahui sebuah kubus ABCD.EFGH dengan panjang rusuk 2√3 cm. Berapakah jarak titik A ke titik G?",
+    options: [
       "3",
-      "4",
-      "5",
+      "6",
+      "2√6",
+      "2√3",
+      "6√3",
     ],
-    answer: 3,
-  },
-  // Nomer 4
-  {
-    q: "Hak asasi yang diatur dalam pembukaan UUD 1945 pada Alinea pertama adalah",
-    options: [
-      "Hak membentuk negara",
-      "Hak menjadi warga negara",
-      "Hak Merdeka",
-      "Hak memeluk agama dan menjalankan agama",
-      "Hak pendidikan",
-    ],
-    answer: 2,
-  },
-  // Nomer 5
-  {
-    q: "Kapolri diangkat dan diberhentikan oleh Presiden dengan persetujuan oleh DPR. Hal ini diatur di dalam",
-    options: [
-      "UUD 1945",
-      "UU No. 2 Tahun 2002",
-      "UU No. 28 Tahun 1987",
-      "UU No. 14 Tahun 2022",
-      "UU No. 39 Tahun 1999",
-    ],
+
     answer: 1,
   },
-  // Nomer 6
-  {
-    q: "Calon Kapolri adalah Perwira Tinggi Kepolisian Negara Republik Indonesia yang masih aktif dengan mempertimbangkan:",
-    options: [
-      "Jenjang, kepangkatan, dan karier",
-      "Usulan dari Menteri Hukum dan HAM",
-      "Usulan dari Kapolri sebelumnya",
-      "Opsi A, B dan C benar",
-      "Opsi A, B dan C tidak ada yang benar",
-    ],
-    answer: 0,
-  },
-  // Nomer 7
-  {
-    q: "Markas polisi yang berkedudukan di wilayah kabupaten adalah",
-    options: [
-      "Polda",
-      "Polres",
-      "Polsek",
-      "Polri",
-      "Polwil",
-    ],
-    answer: 1,
-  },
-  // Nomer 8
-  {
-    q: "Ibu kota negara Brazil adalah",
-    options: [
-      "Rio De Janeiro",
-      "Sao Paolo",
-      "Brasilia",
-      "Salvador",
-      "Franca",
-    ],
-    answer: 2,
-  },
-  // Nomer 9
-  {
-    q: "POLRI resmi terpisah dari ABRI pada tahun",
-    options: [
-      "1962",
-      "1998",
-      "1999",
-      "2000",
-      "1947",
-    ],
-    answer: 2,
-  },
-  // Nomer 10
-  {
-    q: "Berikut adalah negara-negara pecahan dari Uni Soviet, kecuali",
-    options: [
-      "Kroasia",
-      "Uzbekistan",
-      "Turkmenistan",
-      "Moldova",
-      "Latvia",
-    ],
-    answer: 0,
-  },
-  // Nomer 11
-  {
-    q: "Pada tahun berapa Papua akhirnya sepenuhnya bergabung menjadi bagian dari wilayah NKRI",
-    options: [
-      "1945",
-      "1949",
-      "1950",
-      "1962",
-      "1963",
-    ],
-    answer: 4,
-  },
-  // Nomer 12
-  {
-    q: "Berikut yang merupakan teori-teori masuknya Kerajaan Hindu-Buddha ke Nusantara, kecuali",
-    options: [
-      "Teori Brahmana",
-      "Teori Ksatria",
-      "Teori Waisya",
-      "Teori Sudra",
-      "Teori Arus Balik",
-    ],
-    answer: 3,
-  },
-  // Nomer 13
-  {
-    q: "Berbeda dengan Kerajaan bercorak Hindu atau Buddha yang menganut bahwa raja merupakan perwujudan dari dewa, Kerajaan dengan corak islam meyakini bahwa raja merupakan wakil dari Tuhan di Dunia (bukan sebagai perwujudannya), hal ini tercermin dari gelar",
-    options: [
-      "Sultan",
-      "Sunan",
-      "Khalifatullah",
-      "Panembahan",
-      "Panatagama",
-    ],
-    answer: 0,
-  },
-  // Nomer 14
-  {
-    q: "Berikut yang merupakan tujuan diajukannya petisi Soetardjo dalam sidang Volksraad di tahun 1937 adalah",
-    options: [
-      "Memperjuangkan kemudahan bagi berdirinya partai-partai politik",
-      "Menolak kebijakan pemerintah Belanda untuk merekrut tentara pribumi",
-      "Menuntut adanya pemilihan langsung bagi anggota Volksraad dari golongan Bumiputera",
-      "Menuntut Belanda agar Indonesia diberi parlemen yang mandiri",
-      "Menuntut agar diperbolehkannya dikumandangkan lagu Indonesia Raya",
-    ],
-    answer: 3,
-  },
-  // Nomer 15
-  {
-    q: "Kerajaan di Nusantara yang pertama kali melakukan kontak dengan ekspedisi Belanda adalah Kerajaan",
-    options: [
-      "Gowa",
-      "Banten",
-      "Mataram",
-      "Maluku",
-      "Malaka",
-    ],
-    answer: 1,
-  },
-  // Nomer 16
-  {
-    q: "<br>(1) Semangat Reconquesta <br>(2) Buku Umago Mundi <br>(3) Teori Heliosentris <br>(4) Kekalahan Kekaisaran Ottoman <br>Dari peristiwa-peristiwa berikut, yang menjadi faktor penjelajahan bangsa Eropa ke benua-benua lainnya adalah:",
-    options: [
-      "(1) dan (3)",
-      "(2) dan (4)",
-      "(1), (2), dan (3)",
-      "(1), (2), (3), dan (4)",
-      "(3) dan (4)",
-    ],
-    answer: 2,
-  },
-  // Nomer 17
-  {
-    q: "Agresi militer Belanda I merupakan wujud dari pelanggaran perjanjian",
-    options: [
-      "Roem Royem",
-      "Bongaya",
-      "Linggarjati",
-      "Renville",
-      "KMB",
-    ],
-    answer: 2,
-  },
-  // Nomer 18
-  {
-    q: "Untuk pertama kalinya, lagu “Indonesia Raya” dikumandangkan di depan umum ketika",
-    options: [
-      "Proklamasi kemerdekaan Indonesia",
-      "Peresmian BPUPKI",
-      "Kongres Pemuda I",
-      "Kongres Pemuda II",
-      "Perjanjian Kalijati",
-    ],
-    answer: 3,
-  },
-  // Nomer 19
-  {
-    q: "Berikut yang merupakan hasil dari diadakannya Kongres Pemuda I adalah kecuali",
-    options: [
-      "Mendesak Belanda membentuk badan perwakilan rakyat",
-      "Menyegerakan hubungan antar organisasi pemuda",
-      "Menyatukan organisasi pemuda",
-      "Merencanakan Kongres Pemuda II",
-      "Mengedepankan paham persatuan",
-    ],
-    answer: 0,
-  },
-  // Nomer 20
-  {
-    q: "Yang merupakan sungai terpanjang di Indonesia adalah",
-    options: [
-      "Sungai Nil",
-      "Sungai Kapuas",
-      "Sungai Mahakam",
-      "Sungai Barito",
-      "Sungai Musi",
-    ],
-    answer: 1,
-  },
-  // Nomer 21
-  {
-    q: "Motif utama dari pendudukan militer Jepang di Indonesia adalah",
-    options: [
-      "Membebaskan bangsa Asia dari imperialism Eropa",
-      "Sikap superioritas dari Jepang",
-      "Keinginan memerdekakan Indonesia dari Bangsa Sekutu",
-      "Melakukan perdagangan dan monopoli pasar",
-      "Membentuk persemakmuran Asia Timur Raya yang dipimpin Jepang",
-    ],
-    answer: 4,
-  },
-  // Nomer 22
-  {
-    q: "Salah satu kebijakan sosial budaya Jepang kepada penduduk Indonesia pada masa penjajahannya adalah kewajiban untuk membungkukkan diri menghadap matahari terbit yang dikenal dengan istilah",
-    options: [
-      "Harakiri",
-      "Seikerei",
-      "Keibodan",
-      "Jugun Ianfu",
-      "Tonarigumi",
-    ],
-    answer: 1,
-  },
-  // Nomer 23
-  {
-    q: "(1) adanya wilayah <br>(2) adanya penduduk <br>(3) Adanya Konstitusi <br>(4) Adanya Pengakuan dari negara lain <br>(5) Adanya Pemerintahan <br>Berikut yang merupakan syarat konstitutif dari berdirinya suatu negara adalah",
-    options: [
-      "(1), (2), dan (3)",
-      "(2), (4), dan (5)",
-      "(1), (3), dan (5)",
-      "(1), (2), dan (5)",
-      "(3), (4) dan (5)",
-    ],
-    answer: 3,
-  },
-  // Nomer 24
-  {
-    q: "Berikut yang merupakan tuntutan-tuntutan mahasiswa pada peristiwa reformasi 1998, kecuali",
-    options: [
-      "Ciptakan pemerintahan yang bersih dari KKN",
-      "Laksanakan otonomi daerah yang seluas-luasnya",
-      "Hapuskan dwifungsi ABRI",
-      "Dihapusnya Dewan Perwakilan Rakyat dan dibentuknya Dewan Perwakilan Rakyat Gotong Royong",
-      "Tegakkan supremasi hukum",
-    ],
-    answer: 3,
-  },
-  // Nomer 25
-  {
-    q: "Hari batik nasional diperingati tiap tanggal",
-    options: [
-      "2 Oktober",
-      "9 Desember",
-      "2 Juli",
-      "26 Juni",
-      "2 Mei",
-    ],
-    answer: 0,
-  },
+    // Nomer 30
+    {
+      q: "Sebuah balok ABCD.EFGH dengan ukuran AB = 10 cm, AD = 8 cm, dan DH = 6 cm. Berapakah jarak antara titik F ke garis BG?",
+      options: [
+        "3,2 cm",
+        "2,5 cm",
+        "5,7 cm",
+        "8,4 cm",
+        "2,4 cm",
+      ],
+  
+      answer: 3,
+    },
+  
 ];
